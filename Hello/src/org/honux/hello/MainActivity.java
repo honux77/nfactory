8924f08d7ff921a0554d92de2d6337a0b0706a5f
horong54 <horong54@naver.com>
2014-07-29 05:24:44 +0900

    add button

--- Hello/src/org/honux/hello/MainActivity.java
+++ Hello/src/org/honux/hello/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener( new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "æ»≥Á«œººø‰", Toast.LENGTH_SHORT).show();
+			}        
+        });
     }
 
 
